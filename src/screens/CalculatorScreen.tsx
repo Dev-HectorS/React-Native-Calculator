8b966830281b1d4df3984fba857f9ee7378922c6
Dev-HectorS <hector.shm981@gmail.com>
2021-10-01 10:28:33 -0500

    Update: Set Calculator App

---
+++ src/screens/CalculatorScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const CalculatorScreen = () => {
+   return (
+      <>
+         <View>
+            <Text>CalculatorScreen</Text>
+         </View>
+      </>
+   )
+}
+
+export default CalculatorScreen;
